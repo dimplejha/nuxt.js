b495bc59175d93f5c3dc38fe0a3ec1e2f10285af
Dimple Jha <dimple.jha@olacabs.com>
2022-08-08 09:58:36 +0530

    typescript number string datatypes

--- a.js
+++ a.js
@@ -1,1 +1,15 @@
-console.log(5 + 2);
+"use strict";
+exports.__esModule = true;
+console.log(5 + 9);
+var A = /** @class */ (function () {
+    function A() {
+    }
+    A.prototype.test = function () {
+        console.log("a");
+    };
+    return A;
+}());
+var a1 = new A();
+a1.test();
+var a = 10;
+console.log(a);
--- a.ts
+++ a.ts
@@ -1,1 +1,13 @@
-console.log(5+2)
+console.log(5+9)
+export {}
+class A{
+    test(){
+        console.log("a")
+    }
+}
+
+let a1=new A();
+a1.test()
+
+let a:number=10
+console.log(a)
